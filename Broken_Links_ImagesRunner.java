189e0a8aca8a5db99d5b36f40e0ab65043ddfd7f
Sabbir Ahamed <54003568+SabbirAhamed007@users.noreply.github.com>
2020-08-22 11:37:13 -0400

    Add files via upload

---
+++ Broken_Links_ImagesRunner.java
@@ -0,0 +1,15 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "features", glue = { "StePDEfinatioN" }, plugin = { "html:target/cucumber-html-report",
+		"json:target/cucumber.json", "pretty:target/cucumber-pretty.txt", "usage:target/cucumber-usage.json",
+		"junit:target/cucumber-results.xml" }, tags = { "@tag11" })
+
+public class Broken_Links_ImagesRunner {
+
+}
